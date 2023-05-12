06c3748f669fb9be98b1d14a2e6f74a63e075fd1
sgvertical <triple.phil@protonmail.com>
2023-05-12 10:23:36 -0700

    added wallpaper, formatting and tuning to dev script

--- vbox-apps.sh
+++ vbox-apps.sh
@@ -1,29 +1,48 @@
-# Shell script to deploy config to a new VirtualBox Ubuntu VM
+# Shell script to deploy a dev config to a new VirtualBox Ubuntu VM
 #
+SECTION_BORDER_ART="\n["================================="]\n"
 
-# The new classic
-sudo snap install nvim -y
-# The foundation
-sudo apt-get install curl ripgrep
+printf $SECTION_BORDER_ART
+printf "Dev Env Setup"
+printf $SECTION_BORDER_ART
+
+PACKAGE_INSTALL_LIST="curl ripgrep xclip"
+printf "\nInstalling packages and managers, including node\n"
+sudo apt-get install $PACKAGE_INSTALL_LIST
+sudo snap install nvim
 
 # Build javascript environment
+printf $SECTION_BORDER_ART
+printf "Installing Node Version Manager, enabling immediately\n"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # Update directories immediately
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-printf "Installing latest node via nvm"
+printf $SECTION_BORDER_ART
+printf "Installing latest node via nvm\n"
 nvm install node
 
-# find/generate an ssh key
+printf $SECTION_BORDER_ART
+# ssh keys and agent
 mkdir -p ~/.ssh/
-SSH_CHECK_RESULT=`find ~/.ssh -name "id_rsa"`
-if [ "$SSH_CHECK_RESULT" == "/home/$USERNAME/.ssh/id_rsa" ]
-then
-	printf "key already present\n"
-else
-	ssh-keygen
-	printf "new key\n"
-fi
+printf "Making ssh key (if necessary)\n"
+ssh-keygen 
+printf "\nAdding to agent\n"
+sudo ssh-agent ~/.ssh/id_rsa.pub
+
+printf $SECTION_BORDER_ART
+# connect to github, prompt to add key
+printf "Checking github ssh authentication. 'yes' if prompted to add github server.\n"
+ssh -T git@github.com
+printf "\nIf that didn't work, add the following key to GitHub\n"
+printf "Go here: GitHub.com -> Profile Icon -> Settings -> SSH & GPG Keys -> New SSH Key\n"
+
+printf $SECTION_BORDER_ART
+sudo cat ~/.ssh/id_rsa.pub
+printf $SECTION_BORDER_ART
+read -n 1 -s -r -p "After adding this key to github, press enter to continue"
+
+printf "\n"
