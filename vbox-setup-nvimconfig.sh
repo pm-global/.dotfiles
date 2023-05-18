1776312f2438a073eea99ae2c438f7e1e93d7266
sgvertical <triple.phil@protonmail.com>
2023-05-18 11:39:35 -0700

    Added nvim config and symlinks

---
+++ vbox-setup-nvimconfig.sh
@@ -0,0 +1,19 @@
+#! bin/bash/
+# Setup nvim LSP, mostly ripped from the primeagen
+
+# Folder setup: make initial directory, symlink the settings
+printf "\nInstalling packages, setting up directories\n"
+INSTALL_PACKS="g++"
+sudo apt-get install $INSTALL_PACKS
+mkdir -p $HOME/.config/
+ln -s -f $HOME/.dotfiles/nvim $HOME/.config/nvim
+
+# Packer plugin manager
+printf "\nCloning Packer (nvim package manager) repo\n"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+printf "To finish nvim setup:"
+printf "1) open ~/.config/nvim/lua/dev/packer.sync in nvim"
+printf "2) use the :so command then :PackerSync"
+printf "3) restart nvim, confirm binds, commands, colors, autocomplete
