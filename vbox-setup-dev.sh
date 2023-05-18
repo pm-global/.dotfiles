51c9e06c7bd6b781009906ddf759b7e1f365ec0a
sgvertical <triple.phil@protonmail.com>
2023-05-18 09:05:28 -0700

    Formatting and file rename, ready for testing

---
+++ vbox-setup-dev.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Script to install some developer tools on an ubuntu VM
+# 
+SECTION_BORDER_ART="\n["================================="]\n"
+SECTION_DIVIDER="\n--------------------\n"
+
+printf $SECTION_BORDER_ART #-------------------------------------
+printf "Install dev programs and some config"
+printf $SECTION_BORDER_ART
+
+PACKAGE_INSTALL_LIST="curl ripgrep xclip"
+SNAP_INSTALL_LIST="nvim"
+printf "\nInstalling packages: $PACKAGE_INSTALL_LIST\n" 
+sudo apt-get install $PACKAGE_INSTALL_LIST
+
+printf "$SECTION_DIVIDER" #---------
+printf "\nInstalling snaps: $SNAP_INSTALL_LIST\n"
+sudo snap install $SNAP_INSTALL_LIST
+
+# Get node manager
+printf $SECTION_BORDER_ART #-------------------------------------
+printf "Installing NVM\n"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+printf "$SECTION_DIVIDER" #---------
+# Update environment to immediately install latest node
+printf "Loading nvm and bash completion\n"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+printf $SECTION_BORDER_ART #-------------------------------------
+printf "Install latest node?\n"
+read -n 1 -s -r -p "enter for 'no', any other input will install node: " NODE_CHOICE
+
+if [ "$NODE_CHOICE" != "" ]
+then
+	printf "\nInstalling latest Node\n"
+	nvm install node
+fi
+
+printf $SECTION_BORDER_ART #-------------------------------------
+# ssh keys and agent - interactive to be more useful
+printf "Making ssh key\nWhat folder to use?\n"
+read -r -p "(enter for $HOME/.ssh): " NEW_DIR
+SSH_DIR="$HOME/.ssh"				# this is bash magic
+[ -n "${NEW_DIR}" ] && SSH_DIR=${NEW_DIR}	# for assigning default values
+ 						# https://stackoverflow.com/a/2013732
+mkdir -p $SSH_DIR
+
+printf "$SECTION_DIVIDER" #---------
+read -r -p "SSH Options (enter for 4096-bit): " USR_OPT
+SSH_OPT="-t rsa -b 4096"			# default
+[ -n "${USR_OPT}" ] && SSH_OPT=${USR_OPT} 	# assigns user options to ssh if present, otherwise default
+
+printf "running ssh-keygen options $SSH_OPT\n"
+ssh-keygen $SSH_OPT -f $SSH_DIR/id_rsa 
+
+printf "$SECTION_DIVIDER" #---------
+printf "\nRefreshing agent with new keys.\n"
+eval "$(ssh-agent -s)"
+chmod 700 "$SSH_DIR"
+ssh-add $SSH_DIR/id_rsa
+
+printf $SECTION_BORDER_ART #-------------------------------------
+# connect to github, prompt to add key
+printf "Checking github ssh authentication. 'yes' if prompted to approve new ssh connection.\n"
+ssh -T git@github.com
+printf "\nIf that didn't work, add the following key to GitHub\n"
+printf "Go here: GitHub.com -> Profile Icon -> Settings -> SSH & GPG Keys -> New SSH Key\n"
+
+printf "Displaying public RSA key"
+printf $SECTION_BORDER_ART #-------------------------------------
+sudo cat "$SSH_DIR/id_rsa.pub"
+printf $SECTION_BORDER_ART #-------------------------------------
+read -n 1 -s -r -p "After adding this key to github, propagation takes about ~1 minute, then press enter to continue"
+printf "\n"  # additional \n because the read command doesn't give one
+printf "$SECTION_BORDER_ART"
---
+++ vbox-setup-newguest.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# short script to set up a new Ubuntu VM with VirtualBox Guest Additions
+#	1. Update OS to latest, clean up, set vars
+#	2. Install packages required for Guest Additions, prompt for CD Install
+#	3. If kernel mod is present, setup guest additions
+
+SECTION_BORDER_ART="\n["================================="]\n"
+SECTION_DIVIDER="\n--------------------\n"
+
+printf "$SECTION_BORDER_ART"
+printf "Setup VirtualBox Ubuntu VMs"
+printf "$SECTION_BORDER_ART"
+
+# first updates
+printf "Updating to latest sources...\n"
+sudo apt-get -qq update -y
+printf "Upgrading to latest packages...\n"
+sudo apt-get -qq dist-upgrade -y
+printf "$SECTION_DIVIDER"
+printf "Some cleanup...\n"
+sudo apt-get -qq autoclean -y
+sudo apt-get -qq autoremove -y
+printf "Initial updates done\n"
+
+# set script vars from updated state
+LINUX_HEADER_VER="linux-headers-$(uname -r)"
+VBOX_REQUIRED_APPS="make gcc $LINUX_HEADER_VER"
+VBOX_GUEST_MODNAME="vboxguest"
+VBOX_CHECK_MODNAME=`lsmod | grep "$VBOX_GUEST_MODNAME" | awk '{print $1}'`
+VBOX_GROUPNAME="vboxsf"
+VBOX_CONFIG_FLAG=0
+
+# prep vm for VirtualBox Guest Additions
+printf $SECTION_BORDER_ART
+printf "Installing these packages required by Guest Additions:\n$VBOX_REQUIRED_APPS\n"
+sudo apt-get -qq install $VBOX_REQUIRED_APPS
+
+# prompt to install Guest Additions CD
+printf $SECTION_BORDER_ART
+printf "From this VM's menu: 'Devices > Insert Guest Additions CD Image...'\n"
+read -n 1 -s -r -p "After the Guest Additions install is complete, press enter to continue."
+printf "\n"
+
+# check that Guest Additions was installed
+printf $SECTION_BORDER_ART
+printf "Checking for VirtualBox Guest Additions\n"
+
+if [ "$VBOX_CHECK_MODNAME" != "$VBOX_GUEST_MODNAME" ]
+then    
+	printf "Guest Addition kernel module "$VBOX_GUEST_MODNAME" not found"
+else	
+	printf "Guest Additions found\n"
+	printf "Adding "$USER" user to "$VBOX_GROUPNAME"\n"
+	sudo adduser --quiet "$USER" "$VBOX_GROUPNAME"
+	printf "The group members of "$VBOX_GROUPNAME" are now:\n"
+	grep -i --color "$VBOX_GROUPNAME" /etc/group
+	printf "$SECTION_DIVIDER"
+fi
+printf "\nA reboot is required to load the kernel modules and update group membership.\n"
+printf "\nConfirm the hypervisor has Guest-Host interaction configured (clipboard, sharing, drag/drop).\n"
+printf "$SECTION_BORDER_ART"
