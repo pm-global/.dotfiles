6f4802103f1b058150c6598070ec08670179b01c
sgvertical <triple.phil@protonmail.com>
2023-05-18 12:09:48 -0700

    Improved git repo setup ux and should switch to ssh

--- vbox-setup-dev.sh
+++ vbox-setup-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to install some developer tools on an ubuntu VM
-# 
+#
 SECTION_BORDER_ART="\n["================================="]\n"
 SECTION_DIVIDER="\n--------------------\n"
 
@@ -10,7 +10,7 @@ printf $SECTION_BORDER_ART
 
 PACKAGE_INSTALL_LIST="curl ripgrep xclip"
 SNAP_INSTALL_LIST="nvim"
-printf "\nInstalling packages: $PACKAGE_INSTALL_LIST\n" 
+printf "\nInstalling packages: $PACKAGE_INSTALL_LIST\n"
 sudo apt-get install $PACKAGE_INSTALL_LIST
 
 printf "$SECTION_DIVIDER" #---------
@@ -54,7 +54,7 @@ SSH_OPT="-t rsa -b 4096"			# default
 [ -n "${USR_OPT}" ] && SSH_OPT=${USR_OPT} 	# assigns user options to ssh if present, otherwise default
 
 printf "running ssh-keygen options $SSH_OPT\n"
-ssh-keygen $SSH_OPT -f $SSH_DIR/id_rsa 
+ssh-keygen $SSH_OPT -f $SSH_DIR/id_rsa
 
 printf "$SECTION_DIVIDER" #---------
 printf "\nRefreshing agent with new keys.\n"
@@ -62,17 +62,24 @@ eval "$(ssh-agent -s)"
 chmod 700 "$SSH_DIR"
 ssh-add $SSH_DIR/id_rsa
 
-printf $SECTION_BORDER_ART #-------------------------------------
-# connect to github, prompt to add key
-printf "Checking github ssh authentication. 'yes' if prompted to approve new ssh connection.\n"
-ssh -T git@github.com
-printf "\nIf that didn't work, add the following key to GitHub\n"
-printf "Go here: GitHub.com -> Profile Icon -> Settings -> SSH & GPG Keys -> New SSH Key\n"
-
 printf "Displaying public RSA key"
 printf $SECTION_BORDER_ART #-------------------------------------
 sudo cat "$SSH_DIR/id_rsa.pub"
 printf $SECTION_BORDER_ART #-------------------------------------
-read -n 1 -s -r -p "After adding this key to github, propagation takes about ~1 minute, then press enter to continue"
+printf "Go here: GitHub.com -> Profile Icon -> Settings -> SSH & GPG Keys -> New SSH Key\n"
+read -n 1 -s -r -p "After adding this key to github, propagation takes about ~1 minute, enter to start a timer and test."
 printf "\n"  # additional \n because the read command doesn't give one
-printf "$SECTION_BORDER_ART"
+
+# timer to wait for github propagation delay
+SECONDS=70
+printf "\nWaiting $SECONDS seconds before testing github\n"
+awk -v t=$SECONDS 'BEGIN{t=int(t*1000); printf "Elapsed Time (HH:MM:SS): %d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}'
+
+printf $SECTION_BORDER_ART #-------------------------------------
+# connect to github, prompt to add key
+printf "Checking github ssh authentication. 'yes' if prompted to approve new ssh connection.\n"
+ssh -T git@github.com
+printf "\nIf that didn't work, check the key on GitHub\n"
+
+printf "\nChanging dotfiles origin from http to ssh\n"
+(cd $HOME/.dotfiles/; git -v git@github.com:sgvertical/.dotfiles.git)
