53e4a0595ad4f0dc34a64c9697da1cc120b3fd3e
none <none@none.com>
2023-05-26 11:18:20 -0700

    moved font stuff into vbox script. dev will hopefully run on most debian

--- vbox-setup-dev.sh
+++ vbox-setup-dev.sh
@@ -8,7 +8,7 @@ printf $SECTION_BORDER_ART #-------------------------------------
 printf "Install dev programs and some config"
 printf $SECTION_BORDER_ART
 
-PACKAGE_INSTALL_LIST="curl ripgrep xclip tmux xz-utils"
+PACKAGE_INSTALL_LIST="curl ripgrep xclip tmux"
 SNAP_INSTALL_LIST="nvim --classic" # looking for that version
 printf "\nInstalling packages: $PACKAGE_INSTALL_LIST\n"
 sudo apt-get install $PACKAGE_INSTALL_LIST
@@ -16,24 +16,6 @@ sudo apt-get install $PACKAGE_INSTALL_LIST
 printf "$SECTION_DIVIDER" #---------
 printf "\nInstalling snaps: $SNAP_INSTALL_LIST\n"
 sudo snap install $SNAP_INSTALL_LIST
-
-printf "$SECTION_DIVIDER" #---------
-printf "\nInstalling patched font\n"
-FONT_PACK="Lilex.tar.xz"
-FONT_LINK="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/Lilex.tar.xz"
-FONT_CLEANUP="*.txt *.md *.xz"
-FONT_FILE="LilexNerdFontMono-Regular.ttf"
-FONT_DIR="$HOME/.fonts/"
-if [ ! -f "$FONT_DIR$FONT_FILE" ];
-    then
-        # bash code to download this file and unpack it.
-        mkdir -p $FONT_DIR
-        wget -nc --directory-prefix=$FONT_DIR $FONT_LINK
-        cd $FONT_DIR && tar -xf $FONT_PACK
-        rm $FONT_CLEANUP
-    else
-        printf "\nFont found, skipping install\n"
-fi
 
 # Get node manager
 printf $SECTION_BORDER_ART #-------------------------------------
@@ -93,5 +75,3 @@ git config --global user.name none
 
 printf "Switch to ssh for repo\n" # pull into private file?
 ( cd ~/.dotfiles ; git remote set-url origin git@github.com:pm-global/.dotfiles.git )
-
-
--- vbox-setup.sh
+++ vbox-setup.sh
@@ -24,11 +24,30 @@ printf "Initial updates done\n"
 
 # set script vars from updated state
 LINUX_HEADER_VER="linux-headers-$(uname -r)"
-VBOX_REQUIRED_APPS="make gcc $LINUX_HEADER_VER"
+VBOX_REQUIRED_APPS="make gcc $LINUX_HEADER_VER xz-utils"
 VBOX_GUEST_MODNAME="vboxguest"
 VBOX_CHECK_MODNAME=`lsmod | grep "$VBOX_GUEST_MODNAME" | awk '{print $1}'`
 VBOX_GROUPNAME="vboxsf"
 VBOX_CONFIG_FLAG=0
+
+# install terminal font
+printf "$SECTION_DIVIDER" #---------
+printf "\nInstalling patched font\n"
+FONT_PACK="Lilex.tar.xz"
+FONT_LINK="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/Lilex.tar.xz"
+FONT_CLEANUP="*.txt *.md *.xz"
+FONT_FILE="LilexNerdFontMono-Regular.ttf"
+FONT_DIR="$HOME/.fonts/"
+if [ ! -f "$FONT_DIR$FONT_FILE" ];
+    then
+        # bash code to download this file and unpack it.
+        mkdir -p $FONT_DIR
+        wget -nc --directory-prefix=$FONT_DIR $FONT_LINK
+        cd $FONT_DIR && tar -xf $FONT_PACK
+        rm $FONT_CLEANUP
+    else
+        printf "\nFont found, skipping install\n"
+fi
 
 # prep vm for VirtualBox Guest Additions
 printf $SECTION_BORDER_ART
@@ -46,9 +65,9 @@ printf $SECTION_BORDER_ART
 printf "Checking for VirtualBox Guest Additions\n"
 
 if [ "$VBOX_CHECK_MODNAME" != "$VBOX_GUEST_MODNAME" ]
-then    
+then
 	printf "Guest Addition kernel module "$VBOX_GUEST_MODNAME" not found"
-else	
+else
 	printf "Guest Additions found\n"
 	printf "Adding "$USER" user to "$VBOX_GROUPNAME"\n"
 	sudo adduser --quiet "$USER" "$VBOX_GROUPNAME"
